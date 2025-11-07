594825131ea85ce9c1ee1fba28f7bd1a0a2aaeba
김준서\rlawn <a01028399047@gmail.com>
2025-11-07 12:42:38 +0900

    [FEAT]스프링 클라우드 게이트웨이 환경으로 프로젝트 재개발

--- src/main/java/com/hulahoop/gateway/controller/UpdateSeat.java
+++ src/main/java/com/hulahoop/gateway/controller/UpdateSeat.java
@@ -11,12 +11,11 @@ import java.util.Map;
 @RestController
 @RequestMapping("/internal")
 public class UpdateSeat {
-
     @PostMapping("/seat-updated")
     public ResponseEntity<Void> handleSeatUpdate(@RequestBody Map<String, Object> payload) {
         Integer scheduleNum = (Integer) payload.get("scheduleNum");
 
-        System.out.println("✅ 좌석 정보 업데이트 감지됨: scheduleNum = " + scheduleNum);
+        System.out.println("✅ 좌석 업데이트 감지됨: scheduleNum = " + scheduleNum);
 
         return ResponseEntity.ok().build();
 }
